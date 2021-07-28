de44d2fc2e4e42ce061f06fe40dd854b9032a730
Manal Ateyyat <manalateyyat4@gmail.com>
2021-07-28 17:02:10 +0300

    updates-2

--- src/components/Main.js
+++ src/components/Main.js
@@ -3,63 +3,72 @@ import HornedBeasts from './HornedBeasts';
 import SelectedBeast from './SelectedBeast';
 import DataJSON from './assets/data.json';
 // import CardColumns  from 'react-bootstrap/CardColumns';
-import Row  from 'react-bootstrap/Row';
-import Modal from 'react-bootstrap/Modal';
+import Row from 'react-bootstrap/Row';
+import Tabs from 'react-bootstrap/Tabs'
 
 class Main extends React.Component {
-  
+
   constructor(props) {
     super(props);
     this.state = {
-      Data: DataJSON
-      
+      Data: DataJSON,
+      name1:'',
+      imageUrl1: '',
+      description1:'',
     }
   }
+  
+  Select = () => {
+    this.setState({
+        name1:this.Data.item.title,
+        imageUrl1: this.Data.item.imageUrl,
+        description1:this.Data.item.description,
+        
+    })
 
-  Big = () => {
-   
-      return(
-        <>
-        
-        <SelectedBeast
-        
-          name= {this.state.name}
-          imageUrl={this.state.imageUrl}
-          description= {this.state.description}/>
-        </>
-        )
-     
-      }
-        
-   
-    
-      
+return(
+<SelectedBeast 
 
-  render() { 
-      return(
 
-   
-        
+name={this.state.name1}
+imageUr={this.state.imageUrl1}
+description={this.state.description1}
+
+/>
+
+ 
+)
+}
+
+
+
+
+
+  render() {
+    return (
+
+
+
       <div>
         <Row xs={1} md={3} className="g-4">
-        {
-          this.state.Data.map(item => {
-            return (
-              <HornedBeasts
-                name={item.title}
-                imageUrl={item.image_url}
-                description={item.description}
-                Big={this.Big}
-              />
-            )
-          })
-        }
+          {
+            this.state.Data.map(item => {
+              return (
+                <HornedBeasts
+                  name={item.title}
+                  imageUrl={item.image_url}
+                  description={item.description}
+                  Big={this.Big}
+                />
+              )
+            })
+          }
         </Row>
       </div>
 
-)
+    )
 
-}
+  }
 }
 ;
 
